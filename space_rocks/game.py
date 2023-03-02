1bcf407f409a8c43d8d04135a3c620f1b9698b24
Amelia Risner <amelia.risner0@gmail.com>
2023-03-02 13:04:31 -0800

    spaceship now disappears upon collision

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -25,7 +25,12 @@ class SpaceRocks:
             self.asteroids.append(Asteroid(position))
 
     def _get_game_objects(self):
-        return [*self.asteroids, self.spaceship]
+        game_objects = [*self.asteroids]
+
+        if self.spaceship:
+            game_objects.append(self.spaceship)
+        
+        return game_objects
 
 
     def main_loop(self): #game loop
@@ -47,16 +52,23 @@ class SpaceRocks:
         
         is_key_pressed = pygame.key.get_pressed()
 
-        if is_key_pressed[pygame.K_RIGHT]:
-            self.spaceship.rotate(clockwise=True)
-        elif is_key_pressed[pygame.K_LEFT]:
-            self.spaceship.rotate(clockwise=False)
-        if is_key_pressed[pygame.K_UP]:
-            self.spaceship.accelerate()
+        if self.spaceship:
+            if is_key_pressed[pygame.K_RIGHT]:
+                self.spaceship.rotate(clockwise=True)
+            elif is_key_pressed[pygame.K_LEFT]:
+                self.spaceship.rotate(clockwise=False)
+            if is_key_pressed[pygame.K_UP]:
+                self.spaceship.accelerate()
 
     def _process_game_logic(self):
         for game_object in self._get_game_objects():
             game_object.move(self.screen)
+
+        if self.spaceship:
+            for asteroid in self.asteroids:
+                if asteroid.collides_with(self.spaceship):
+                    self.spaceship = None
+                    break
 
     def _draw(self):
         self.screen.blit(self.background, (0,0))
